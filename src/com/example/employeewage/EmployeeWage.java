9a4fcb4359935856b5407b7dc727dcbefbf8472a
satoteharshal97 <37376689+satoteharshal97@users.noreply.github.com>
2022-01-03 22:34:07 +0530

    Added - Calculated total wage for different companies

--- src/com/example/employeewage/EmployeeWage.java
+++ src/com/example/employeewage/EmployeeWage.java
@@ -2,64 +2,22 @@ package com.example.employeewage;
 
 public class EmployeeWage {
     public static final int IS_FULL_TIME = 1;
-    public static final int EMP_WAGE_PER_HOUR = 20;
     public static final int IS_PART_TIME = 2;
-    public static final int NUM_OF_WORKING_DAYS = 20;
-    public static final int MAX_HOURS_FOR_MONTH = 100;
-    int totalEmpHours = 0, totalWorkingDays = 0, empHours;
+
+    int totalEmpHours = 0, totalWorkingDays = 0, totalEmpWage = 0,  empHours, wagePerHour, numberOfWorkingDays, maxHours;
 
     public static void main(String[] args) {
-        EmployeeWage switchFullOrPart = new EmployeeWage();
-        System.out.println("Employee Wage until condition after using class variables :" + switchFullOrPart.getMonthlyWageCondition());
-
-    }
-    public int getMonthlyWageCondition(){
-
-
-        while (totalEmpHours <= MAX_HOURS_FOR_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS){
-            totalWorkingDays++;
-            int empChecker = (int)Math.floor(Math.random() * 10) % 3;
-            switch(empChecker){
-                case IS_FULL_TIME:
-                    empHours = 8;
-                    break;
-                case IS_PART_TIME:
-                    empHours = 4;
-                    break;
-                default:
-                    empHours = 0;
-            }
-            totalEmpHours = totalEmpHours + empHours;
-        }
-        int totalEmpWage = totalEmpHours * EMP_WAGE_PER_HOUR;
-        return totalEmpWage;
+        EmployeeWage object = new EmployeeWage();
+        System.out.println("For Dmart: " + object.getMonthlyWage(8, 20, 100));
+        System.out.println("For Reliance: " + object.getMonthlyWage(6, 30, 150));
+        System.out.println("For TCS: " + object.getMonthlyWage(10, 26, 200));
     }
 
-    public int getMonthlyWage(){
-        int empHours, empWage, totalEmpWage = 0;
-        for(int day = 1; day <= NUM_OF_WORKING_DAYS;  day++){
-            int empChecker = (int)Math.floor(Math.random() * 10) % 3;
+    public int getMonthlyWage(int wagePerHour, int numberOfWorkingDays, int maxHours) {
 
-            switch(empChecker){
-                case IS_FULL_TIME:
-                    empHours = 8;
-                    break;
-                case IS_PART_TIME:
-                    empHours = 4;
-                    break;
-                default:
-                    empHours = 0;
-            }
-            empWage = empHours * EMP_WAGE_PER_HOUR;
-            totalEmpWage = totalEmpWage + empWage;
-            System.out.println("Employee wage for " + day + " is " + empWage);
-        }
-        return totalEmpWage;
-    }
-
-    public int switchGetFullOrPartTimeWage() {
-        int empHours;
+        while(totalEmpHours <= maxHours && totalWorkingDays < numberOfWorkingDays){
         int empChecker = (int) Math.floor(Math.random() * 10) % 3;
+
         switch (empChecker) {
             case IS_FULL_TIME:
                 empHours = 8;
@@ -70,46 +28,9 @@ public class EmployeeWage {
             default:
                 empHours = 0;
         }
-        return empHours * EMP_WAGE_PER_HOUR;
-    }
-
-    public  int getFullOrPartTimeWage(){
-        int empHours;
-        double empChecker = (int)Math.floor(Math.random() * 10) % 3;
-
-        if(empChecker == IS_FULL_TIME) {
-            empHours = 8;
+        totalEmpHours = totalEmpHours + empHours;
         }
-        else if(empChecker == IS_PART_TIME){
-            empHours = 4;
-        }
-        else {
-            empHours = 0;
-        }
-        return empHours * EMP_WAGE_PER_HOUR;
-    }
-
-    public  int dailyWage(){
-        int empHours;
-        int empWage;
-        double empChecker = (int)Math.floor(Math.random() * 10) % 2;
-        if(empChecker == IS_FULL_TIME) {
-            empHours = 8;
-        }
-        else {
-            empHours = 0;
-        }
-        empWage = empHours * EMP_WAGE_PER_HOUR;
-        return empWage;
-    }
-
-    static void checkAbsentOrPresent(){
-        double empChecker = (int)Math.floor(Math.random() * 10) % 2;
-        if(empChecker == IS_FULL_TIME) {
-            System.out.println("Employee is Present");
-        }
-        else {
-            System.out.println("Employee is Absent");
-        }
+        int totalEmpWage = totalEmpHours * wagePerHour;
+        return totalEmpWage;
     }
 }
