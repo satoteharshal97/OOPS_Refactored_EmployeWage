7edb78d3dbc0cd0929794659847f7dafc1e04716
satoteharshal97 <37376689+satoteharshal97@users.noreply.github.com>
2022-01-03 21:01:35 +0530

    Added - Get employee wage for month

--- src/com/example/employeewage/EmployeeWage.java
+++ src/com/example/employeewage/EmployeeWage.java
@@ -4,13 +4,35 @@ public class EmployeeWage {
     public static final int IS_FULL_TIME = 1;
     public static final int EMP_WAGE_PER_HOUR = 20;
     public static final int IS_PART_TIME = 2;
+    public static final int NUM_OF_WORKING_DAYS = 20;
 
 
     public static void main(String[] args) {
         EmployeeWage switchFullOrPart = new EmployeeWage();
-        System.out.println("Employee Wage :" + switchFullOrPart.switchGetFullOrPartTimeWage());
-    }
+        System.out.println("Employee Wage :" + switchFullOrPart.getMonthlyWage());
 
+    }
+    public int getMonthlyWage(){
+        int empHours, empWage, totalEmpWage = 0;
+        for(int day = 1; day <= NUM_OF_WORKING_DAYS;  day++){
+            int empChecker = (int)Math.floor(Math.random() * 10) % 3;
+
+            switch(empChecker){
+                case IS_FULL_TIME:
+                    empHours = 8;
+                    break;
+                case IS_PART_TIME:
+                    empHours = 4;
+                    break;
+                default:
+                    empHours = 0;
+            }
+            empWage = empHours * EMP_WAGE_PER_HOUR;
+            totalEmpWage = totalEmpWage + empWage;
+            System.out.println("Employee wage for " + day + " is " + empWage);
+        }
+        return totalEmpWage;
+    }
     public int switchGetFullOrPartTimeWage() {
         int empHours;
         int empChecker = (int) Math.floor(Math.random() * 10) % 3;
@@ -30,7 +52,6 @@ public class EmployeeWage {
 
     public  int getFullOrPartTimeWage(){
         int empHours;
-        int empWage;
         double empChecker = (int)Math.floor(Math.random() * 10) % 3;
 
         if(empChecker == IS_FULL_TIME) {
