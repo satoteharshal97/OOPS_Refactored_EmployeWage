d926c3f3dcf70e8fcf1c692533e8be897d64a5a2
satoteharshal97 <37376689+satoteharshal97@users.noreply.github.com>
2022-01-03 19:49:47 +0530

    Added - Calculate daily wage

--- src/com/example/employeewage/EmployeeWage.java
+++ src/com/example/employeewage/EmployeeWage.java
@@ -2,11 +2,25 @@ package com.example.employeewage;
 
 public class EmployeeWage {
     public static final int IS_FULL_TIME = 1;
+    public static final int EMP_WAGE_PER_HOUR = 20;
 
     public static void main(String[] args) {
-        checkAbsentOrPresent();
+        EmployeeWage fullTimeWage = new EmployeeWage();
+        System.out.println(fullTimeWage.dailyWage());
     }
-
+    public  int dailyWage(){
+        int empHours;
+        int empWage;
+        double empChecker = (int)Math.floor(Math.random() * 10) % 2;
+        if(empChecker == IS_FULL_TIME) {
+            empHours = 8;
+        }
+        else {
+            empHours = 0;
+        }
+        empWage = empHours * EMP_WAGE_PER_HOUR;
+        return empWage;
+    }
     static void checkAbsentOrPresent(){
         double empChecker = (int)Math.floor(Math.random() * 10) % 2;
         if(empChecker == IS_FULL_TIME) {
