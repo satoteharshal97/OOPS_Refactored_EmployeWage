6c289bf863be2d42b1be99b65a02830234b17d23
satoteharshal97 <37376689+satoteharshal97@users.noreply.github.com>
2022-01-04 08:41:11 +0530

    Refactored - Changed Indentation in code

--- src/com/example/employeewage/EmployeeWage.java
+++ src/com/example/employeewage/EmployeeWage.java
@@ -4,7 +4,7 @@ public class EmployeeWage {
     public static final int IS_FULL_TIME = 1;
     public static final int IS_PART_TIME = 2;
 
-    int totalEmpHours = 0, totalWorkingDays = 0, totalEmpWage = 0,  empHours, wagePerHour, numberOfWorkingDays, maxHours;
+    int totalEmpHours = 0, totalWorkingDays = 0, totalEmpWage = 0, empHours, wagePerHour, numberOfWorkingDays, maxHours;
 
     public static void main(String[] args) {
         EmployeeWage object = new EmployeeWage();
@@ -15,20 +15,20 @@ public class EmployeeWage {
 
     public int getMonthlyWage(int wagePerHour, int numberOfWorkingDays, int maxHours) {
 
-        while(totalEmpHours <= maxHours && totalWorkingDays < numberOfWorkingDays){
-        int empChecker = (int) Math.floor(Math.random() * 10) % 3;
+        while (totalEmpHours <= maxHours && totalWorkingDays < numberOfWorkingDays) {
+            int empChecker = (int) Math.floor(Math.random() * 10) % 3;
 
-        switch (empChecker) {
-            case IS_FULL_TIME:
-                empHours = 8;
-                break;
-            case IS_PART_TIME:
-                empHours = 4;
-                break;
-            default:
-                empHours = 0;
-        }
-        totalEmpHours = totalEmpHours + empHours;
+            switch (empChecker) {
+                case IS_FULL_TIME:
+                    empHours = 8;
+                    break;
+                case IS_PART_TIME:
+                    empHours = 4;
+                    break;
+                default:
+                    empHours = 0;
+            }
+            totalEmpHours = totalEmpHours + empHours;
         }
         int totalEmpWage = totalEmpHours * wagePerHour;
         return totalEmpWage;
